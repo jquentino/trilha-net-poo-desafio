dd953e2eb3ab29b70490ab18309fc15bfdf868ee
jquentino <vitorquentino@gmail.com>
2024-10-05 17:32:50 -0300

    implementa classes nokia e iphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new(numero: "9999-9999", modelo: "XI", imei: "123", memoria: 100);
+Nokia nokia = new(numero: "9999-9991", modelo: "N96", imei: "456", memoria: 10);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("RadarBot");
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake Game");
